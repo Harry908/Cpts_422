1f09ef278cb84ab98573ae5590f696db73f2c486
Huy Ky <huyky26@gmail.com>
2024-09-22 10:23:39 -0700

    Add project

---
+++ FirstPlugIn/src/CheckPlugIn/SemiColonCountCheck.java
@@ -0,0 +1,42 @@
+package CheckPlugIn;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+public class SemiColonCountCheck extends AbstractCheck {
+  
+  private int count = 0;
+
+  @Override
+  public int[] getAcceptableTokens() {
+    return new int[] {TokenTypes.SEMI };
+  }
+
+  @Override
+  public int[] getRequiredTokens() {
+    return new int[0];
+  }
+
+  @Override
+  public int[] getDefaultTokens() {
+    return new int[] { TokenTypes.SEMI };
+  }
+
+  @Override
+  public void visitToken(DetailAST ast) {
+    count++;
+  }
+  @Override
+  public void beginTree(DetailAST ast)
+  {
+	  count = 0;
+  }
+ 
+  @Override
+  public void finishTree(DetailAST ast)
+  {  
+	  log(ast.getLineNo(), "Semi Colon Count "+ count);
+  }
+}
+
