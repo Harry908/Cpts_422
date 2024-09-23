5bf2c3b346bb668ec2dd12477e078465f0f73b8f
Huy Ky <huyky26@gmail.com>
2024-09-22 21:16:13 -0700

    Add junit dependencies

---
+++ FirstPlugIn/src/main/test/Tests/CheckTest.java
@@ -0,0 +1,13 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+public class CheckTest {
+	
+	@Test
+	public void Test1() {
+		assertEquals(5,5);
+	}
+}
