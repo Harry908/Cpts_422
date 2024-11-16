422aff6b1b003e4191c91c0c6538b452e7c5141d
Huy Ky <huyky26@gmail.com>
2024-11-16 13:11:54 -0800

    Add HalsteadVolume implementation and tests

--- FirstPlugIn/src/main/java/aChecks/HalsteadVolumeCheck.java
+++ FirstPlugIn/src/main/java/aChecks/HalsteadVolumeCheck.java
@@ -1,5 +1,49 @@
 package aChecks;
+import java.util.HashSet;
+import java.util.Set;
 
-public class HalsteadVolumeCheck {
+import com.puppycrawl.tools.checkstyle.api.*;
+public class HalsteadVolumeCheck extends AbstractCheck {
+	
+	private Set<String> visitedTokens;
+	private int length = 0;
+	@Override
+	public int[] getAcceptableTokens() {
+	    return HalsteadToken.ALL_TOKENS;
+	}
 
+	@Override
+	public int[] getRequiredTokens() {
+		return new int[0];
+	}
+	
+	@Override 
+	public int[] getDefaultTokens() {  
+	    return getAcceptableTokens();
+	}
+	
+	@Override
+	public void visitToken(DetailAST ast) {
+		length++;
+		visitedTokens.add(ast.getText());
+	}
+	@Override
+	public void beginTree(DetailAST ast)
+	{
+		length = 0;
+		visitedTokens = new HashSet<>();
+	}
+	 
+	@Override
+	public void finishTree(DetailAST ast)
+	{  
+		// Volume = N*log2(n)
+		// N = length 
+		// log2(n)= log(n) / log(2)
+		int vocabulary = visitedTokens.size(); // n
+		double volume = length * (Math.log(vocabulary) / Math.log(2));
+		String formattedVolume = String.format("%.2f", volume);
+		
+		log(ast.getLineNo(), "Halstead Volume: "+ formattedVolume + " -HK");
+	}
 }
---
+++ FirstPlugIn/src/test/java/aChecksTests/HalsteadVolumeTest.java
@@ -0,0 +1,79 @@
+package aChecksTests;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+
+import aChecks.*;
+
+public class HalsteadVolumeTest {
+	private HalsteadVolumeCheck testCheck;
+    private DetailAST mockDetailAST;
+
+    @BeforeEach
+    public void setUp() {
+        testCheck = spy(new HalsteadVolumeCheck());
+        mockDetailAST = mock(DetailAST.class);
+        // Initialize the tree
+        testCheck.beginTree(mockDetailAST);
+    }
+
+    @Test
+    public void testGetTokens()
+    {
+
+    	assertArrayEquals(HalsteadToken.ALL_TOKENS, testCheck.getAcceptableTokens());
+    	assertArrayEquals(new int [0], testCheck.getRequiredTokens());
+    	assertArrayEquals(HalsteadToken.ALL_TOKENS, testCheck.getDefaultTokens());
+    }
+    
+    @Test
+    public void testVisitToken() {
+    	// Mock the behavior of DetailAST
+    	when(mockDetailAST.getText()).thenReturn("+");
+        testCheck.visitToken(mockDetailAST);
+    	// Visit token
+        verify(testCheck).visitToken(mockDetailAST);
+    }
+
+    @Test
+    public void testBeginTree()
+    {
+    	verify(testCheck).beginTree(mockDetailAST);
+    }
+    
+    @Test
+    public void testFinishTree() {        
+        // Mock the behavior of DetailAST
+        when(mockDetailAST.getLineNo()).thenReturn(1);
+
+        // Add tokens
+        when(mockDetailAST.getText()).thenReturn("+");
+        testCheck.visitToken(mockDetailAST);
+        when(mockDetailAST.getText()).thenReturn("+");
+        testCheck.visitToken(mockDetailAST);
+        when(mockDetailAST.getText()).thenReturn("-");
+        testCheck.visitToken(mockDetailAST);
+        when(mockDetailAST.getText()).thenReturn("sort");
+        testCheck.visitToken(mockDetailAST);
+        when(mockDetailAST.getText()).thenReturn("b");
+        testCheck.visitToken(mockDetailAST);
+        
+        // Calculate volume
+        int N = 5;
+        int n = 4;
+        double volume = N * (Math.log(n) / Math.log(2));
+		String formattedVolume = String.format("%.2f", volume);
+        
+        // Finish the tree and log the result
+        doNothing().when(testCheck).log(anyInt(), anyString());
+        testCheck.finishTree(mockDetailAST);
+        
+        // Verify log is called and argument passed in
+        verify(testCheck).log(eq(1),contains(formattedVolume));
+    }
+}
